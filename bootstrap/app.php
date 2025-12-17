4f1c11defa22222621296b8001368583ded7bb3a
ryanditata <ryanditata38@gmail.com>
2025-12-17 14:10:47 +0700

    update midtrans

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
+        $middleware->validateCsrfTokens(except: [
+            'checkout/notification', 
+        ]);
+
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
