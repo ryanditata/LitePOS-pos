6bfed887feccc7d02902b078fd68fdade85b7634
ryanditata <ryanditata38@gmail.com>
2025-12-18 10:56:55 +0700

    update ui auth

---
+++ resources/js/components/app-logo2.tsx
@@ -0,0 +1,14 @@
+export default function AppLogo2() {
+    return (
+        <>
+            <div className="flex items-center">
+                <div className="w-24 h-auto">
+                    <img src="/images/logo-navbar2.png" alt="Icon Dashboard" />
+                </div>
+                <div className="ml-2 grid flex-1 text-left text-base">
+                    <span className="truncate leading-tight font-semibold">LitePOS</span>
+                </div>
+            </div>
+        </>
+    );
+}
--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,4 +1,4 @@
-import AppLogoIcon from '@/components/app-logo-icon';
+import AppLogoIcon from '@/components/app-logo2';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
@@ -11,7 +11,7 @@ interface AuthLayoutProps {
 export default function AuthSimpleLayout({ children, title, description }: PropsWithChildren<AuthLayoutProps>) {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-            <div className="w-full max-w-sm">
+            <div className="w-full max-w-sm border-2 rounded-xl p-5">
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
